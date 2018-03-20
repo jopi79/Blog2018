ca9c3c4eccda160501b4eb49bb4ae0e7737730e5
jopi79 <Student@193.107.34.194>
2018-03-20 11:47:48 +0100

    sortowanie komentarzy

--- Blog2018/src/blog2018/NewMain1.java
+++ Blog2018/src/blog2018/NewMain1.java
@@ -19,9 +19,9 @@ public class NewMain1 {
      */
     public static void main(String[] args) {
         
-        Comment c = new Comment("adam","jakiś tekst", LocalDateTime.of(2018, 3, 19, 8, 0, 0));
+        Comment c = new Comment("adam","jakiś tekst");
         Comment c2 = new Comment("bernard","jakiś tekst", LocalDateTime.of(2018, 3, 19, 10, 0, 0));
-        Comment c3 = new Comment("celina","jakiś tekst");
+        Comment c3 = new Comment("celina","jakiś tekst", LocalDateTime.of(2018, 3, 19, 8, 0, 0));
         
         System.out.println(c);
         System.out.println(c2);
@@ -32,6 +32,13 @@ public class NewMain1 {
         set.add(c2);
         set.add(c3);
         System.out.println(set);
+        
+        CommentComparator cc = new CommentComparator();
+        TreeSet set2 = new TreeSet(cc);
+        set2.add(c);
+        set2.add(c2);
+        set2.add(c3);
+        System.out.println(set2);
         
     }
     
